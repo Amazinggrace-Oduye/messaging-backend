7ecacf5915faee48789ac0c16972e2517147d8dd
Amazinggrace <grace.oduye@gmail.com>
2022-11-22 10:39:38 +0100

    validation check

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  Query,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { CreatemessageDto } from './dtos/create-messages.dto';
 
 @Controller('messages')
 export class MessagesController {
+  @Get('/:id')
+  getMessage(@Param('id', ParseIntPipe) id: number, @Query() query: string) {
+    return `am the messsage with id:${id} and query:${query}`;
+  }
+  @Post()
+  createMessage(@Body() body: CreatemessageDto) {
+    return `${body}`;
+  }
   @Get()
-  getMessage(): string {
-    return 'am the messsage';
+  listMessages(): string[] {
+    return ['hello', 'you'];
   }
 }
